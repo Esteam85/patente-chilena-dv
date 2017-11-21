6dcc01e42fe313907f08aaf7bed4928d8c3b009a
esteam85 <contacto@econtreras.net>
2017-11-21 11:05:23 -0300

    Refactor to class style and jest test folder structures

---
+++ __tests__/transformInPreNumer.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe("transformInPreNumer | Test",()=>{
+
+  test("should fail if not recognice ", () =>{
+    // Reconocer Typo de transformación por tipo de ppu.
+
+
+
+  });
+
+});
--- index.js
+++ index.js
@@ -2,60 +2,75 @@
 const _ = require('lodash');
 const _variables = require('./variables.json');
 
-let index = {
-  verifyPpuType: (ppu) => {
+
+class Index{
+
+  constructor(){
+
+  }
+
+  static verifyPpuType(ppu){
     if (ppu.length !== 6) {
       throw new Error("La patente(ppu) ingresada es de largo inválido")
     }
     // New Moto Format
-    if (_.includes(ppu, '0',3) && isMoto(ppu)) {
+    if (_.includes(ppu, '0',3) && this.isMoto(ppu)) {
       return _variables.motoNewFormat;
     }
 
     // Old Moto Format
-    if (_.includes(ppu, '0',2) && isMoto(ppu)) {
+    if (_.includes(ppu, '0',2) && this.isMoto(ppu)) {
       return _variables.motoOldFormat;
     }
 
     // New Auto Format
-    if (isAutoMovilNew(ppu)) {
+    if (this.isAutoMovilNew(ppu)) {
       return _variables.autoNewFormat;
     }
     // Old Auto Format
-    if (isAutoMovilOld(ppu)) {
+    if (this.isAutoMovilOld(ppu)) {
       return _variables.autoOldFormat;
     }
     // Carro Arrastre Format
-    if (isCarroArrastre(ppu)) {
+    if (this.isCarroArrastre(ppu)) {
       return _variables.carroArrastreFormat;
     }
     throw new Error("Ppu no corresponde a ningun formato válido")
-  },
-  transformInPreNumer: () => {
-  },
-  calculateDV: (prePpu) => {
+  }
+
+  static transformInPreNumer(ppu) {
+    // Reconocer Typo de transformación por tipo de ppu
+
+    // Cambiar letras por números (transformación) dependiendo tipo de ppu
+
+    // retornar numeros.
+
+  }
+
+  static calculateDV (prePpu) {
+  }
+
+  static isAutoMovilNew(ppu) {
+    // Validate format XXXX11
+    const regexAutomovil = new RegExp(/^[b-d,f-h,j-l,p,r-t,v-z]{2}[\-\. ]?[b-d,f-h,j-l,p,r-t,v-z]{2}[\.\- ]?[0-9]{2}$/, 'i');
+    return regexAutomovil.test(ppu);
+  }
+  static isAutoMovilOld(ppu) {
+    // Validate format XX1111
+    const regexAutomovil = new RegExp(/^[a-z]{2}[\.\- ]?[0-9]{2}[\.\- ]?[0-9]{2}$/, 'i');
+    return regexAutomovil.test(ppu);
+  }
+
+  static isMoto(ppu) {
+    // Validate format XX0111 / XX111 / XXX011 / XXX11
+    const regexMoto = new RegExp(/^[a-z]{2}0[0-9]{3}$|^[a-z]{2}[0-9]{3}$|^[b-d,f-h,j-l,p,r-t,v-z]{3}[0-9]{2}$|^[b-d,f-h,j-l,p,r-t,v-z]{3}0[0-9]{2}$/, 'i');
+    return regexMoto.test(ppu);
+  }
+  static isCarroArrastre(ppu) {
+    // Validate format XXX111
+    const regexCarroArrastre = new RegExp(/^[a-zA-Z]{3}[1-9]{1}[0-9]{2}$/, 'i');
+    return regexCarroArrastre.test(ppu);
   }
 }
 
-function isAutoMovilNew(ppu) {
-  // Validate format XXXX11
-  const regexAutomovil = new RegExp(/^[b-d,f-h,j-l,p,r-t,v-z]{2}[\-\. ]?[b-d,f-h,j-l,p,r-t,v-z]{2}[\.\- ]?[0-9]{2}$/, 'i');
-  return regexAutomovil.test(ppu);
-}
-function isAutoMovilOld(ppu) {
-  // Validate format XX1111
-  const regexAutomovil = new RegExp(/^[a-z]{2}[\.\- ]?[0-9]{2}[\.\- ]?[0-9]{2}$/, 'i');
-  return regexAutomovil.test(ppu);
-}
-
-function isMoto(ppu) {
-  // Validate format XX0111 / XX111 / XXX011 / XXX11
-  const regexMoto = new RegExp(/^[a-z]{2}0[0-9]{3}$|^[a-z]{2}[0-9]{3}$|^[b-d,f-h,j-l,p,r-t,v-z]{3}[0-9]{2}$|^[b-d,f-h,j-l,p,r-t,v-z]{3}0[0-9]{2}$/, 'i');
-  return regexMoto.test(ppu);
-}
-function isCarroArrastre(ppu) {
-  // Validate format XXX111
-  const regexCarroArrastre = new RegExp(/^[a-zA-Z]{3}[1-9]{1}[0-9]{2}$/, 'i');
-  return regexCarroArrastre.test(ppu);
-}
-module.exports = index;
+module.exports = Index;
