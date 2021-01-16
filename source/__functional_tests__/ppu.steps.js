6a5294e3eb2ab685b3f377bdd6b5034efd246eff
Esteban Contreras <esteban.contreras@walmart.com>
2021-01-16 17:09:54 -0300

    (refactor): updating cucumber to the last versio and updating fet

--- source/__functional_tests__/ppu.steps.js
+++ source/__functional_tests__/ppu.steps.js
@@ -1,28 +1,24 @@
-const {defineSupportCode} = require('cucumber');
-const Ppu = require('../ppu');
-const expect = require('expect');
+const expect = require('expect')
+const { Given, When, Then } = require('@cucumber/cucumber');
+const Ppu = require('../ppu')
 
 
-defineSupportCode(function({Given, Then, When}) {
+Given(/^a valid (.+)$/, function (ppu, callback) {
+  this.ppu = ppu;
+  callback();
+});
 
-  Given(/^a valid (.+)$/, function (ppu, callback) {
-    this.ppu = ppu;
-    callback();
-  });
+When(/^generate the DV$/, function (callback) {
+  this.generatedPpu = new Ppu(this.ppu);
+  callback();
+});
 
-  When(/^generate the DV$/, function (callback) {
-    this.generatedPpu = new Ppu(this.ppu);
-    callback();
-  });
+Then(/^should have the respective (.+) - DV$/, function (dv, callback) {
+  expect(this.generatedPpu.dv).toBe(dv);
+  callback();
+});
 
-  Then(/^should have the respective (.+) - DV$/, function (dv, callback) {
-    expect(this.generatedPpu.dv).toBe(dv);
-    callback();
-  });
-
-  Then(/^should have the respective (.+) - Format$/, function (format, callback) {
-    expect(this.generatedPpu.format.type).toBe(format);
-    callback();
-  });
-
+Then(/^should have the respective (.+) - Format$/, function (format, callback) {
+  expect(this.generatedPpu.format.type).toBe(format);
+  callback();
 });
